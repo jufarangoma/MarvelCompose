2da60b18d1c6d14b3f4adb0a6f4e0a76aaff1f86
Juan Arango <jufarangoma@gmail.com>
2024-04-01 19:00:05 -0500

    Add FirstScreen

---
+++ app/src/main/java/com/jufarangoma/marvelcompose/presentation/ui/screens/FirstScreen.kt
@@ -0,0 +1,44 @@
+package com.jufarangoma.marvelcompose.presentation.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jufarangoma.marvelcompose.R
+import com.jufarangoma.marvelcompose.presentation.navigation.Screens
+
+@Composable
+fun FirstScreen(
+    navigateToScreen: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            stringResource(id = R.string.app_name),
+            modifier = Modifier.fillMaxWidth(),
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center
+        )
+        ElevatedButton(
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onPrimaryContainer),
+            onClick = { navigateToScreen(Screens.SuperHeroesScreen.name) }) {
+            Text(
+                stringResource(id = R.string.start),
+            )
+        }
+    }
+}
